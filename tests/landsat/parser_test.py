4b63ce512d076be813c070dc62539f678b310c8d
Panos Mavrogiorgos <pmav99@gmail.com>
2019-06-05 15:19:52 +0300

    Add tests for get_landsat_*_band() functions.

--- tests/landsat/parser_test.py
+++ tests/landsat/parser_test.py
@@ -6,10 +6,16 @@ import pytest
 
 from rsutils.landsat.parser import (
     parse_mtl,
-    LS8_Metadata,
     TileCoords,
+    LS8_Metadata,
     LS8_Band,
     LS8_ThermalBand,
+    LS8_QABand,
+    LS8_Radiance,
+    LS8_Reflectance,
+    get_landsat_qa_band,
+    get_landsat_band,
+    get_landsat_thermal_band,
 )
 
 
@@ -106,3 +112,58 @@ def test_landasat_8_instance_creation(global_datadir, mtl_filename):
     assert isinstance(ls8.b9, LS8_Band)
     assert isinstance(ls8.b10, LS8_ThermalBand)
     assert isinstance(ls8.b11, LS8_ThermalBand)
+
+
+@LANDSAT_8_FILES
+def test_get_landsat_qa_band(global_datadir, mtl_filename):
+    metadata = parse_mtl(global_datadir / mtl_filename, convert=True)
+    qa_band = get_landsat_qa_band(metadata)
+    assert isinstance(qa_band, LS8_QABand)
+    assert qa_band.index == 0
+    assert qa_band.filename == qa_band.path.name
+    assert not qa_band.path.exists()
+    assert qa_band.height == metadata["reflective_lines"]
+    assert qa_band.width == metadata["reflective_samples"]
+
+
+@pytest.mark.parametrize("band_index", (10, 11))
+@LANDSAT_8_FILES
+def test_get_landsat_thermal_band(global_datadir, mtl_filename, band_index):
+    metadata = parse_mtl(global_datadir / mtl_filename, convert=True)
+    thermal_band = get_landsat_thermal_band(metadata, band_index)
+    assert isinstance(thermal_band, LS8_ThermalBand)
+    assert thermal_band.index == band_index
+    assert thermal_band.filename == thermal_band.path.name
+    assert not thermal_band.path.exists()
+    assert thermal_band.height == metadata["thermal_lines"]
+    assert thermal_band.width == metadata["thermal_samples"]
+    assert isinstance(thermal_band.radiance, LS8_Radiance)
+
+
+@pytest.mark.parametrize("band_index", [1, 2, 3, 4, 5, 6, 7, 9])
+@LANDSAT_8_FILES
+def test_get_landsat_band(global_datadir, mtl_filename, band_index):
+    metadata = parse_mtl(global_datadir / mtl_filename, convert=True)
+    thermal_band = get_landsat_band(metadata, band_index)
+    assert isinstance(thermal_band, LS8_Band)
+    assert thermal_band.index == band_index
+    assert thermal_band.filename == thermal_band.path.name
+    assert not thermal_band.path.exists()
+    assert thermal_band.height == metadata["reflective_lines"]
+    assert thermal_band.width == metadata["reflective_samples"]
+    assert isinstance(thermal_band.radiance, LS8_Radiance)
+    assert isinstance(thermal_band.reflectance, LS8_Reflectance)
+
+
+@LANDSAT_8_FILES
+def test_get_landsat_panchromatic_band(global_datadir, mtl_filename):
+    metadata = parse_mtl(global_datadir / mtl_filename, convert=True)
+    thermal_band = get_landsat_band(metadata, 8)
+    assert isinstance(thermal_band, LS8_Band)
+    assert thermal_band.index == 8
+    assert thermal_band.filename == thermal_band.path.name
+    assert not thermal_band.path.exists()
+    assert thermal_band.height == metadata["panchromatic_lines"]
+    assert thermal_band.width == metadata["panchromatic_samples"]
+    assert isinstance(thermal_band.radiance, LS8_Radiance)
+    assert isinstance(thermal_band.reflectance, LS8_Reflectance)
